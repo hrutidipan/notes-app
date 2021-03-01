12d75a22651c71553873e413f1ada19514d7b820
hrutidipan <hrutidipan@gmail.com>
2021-03-01 13:14:55 +0530

    login authentication

---
+++ (d)JS files/login.js
@@ -0,0 +1,9 @@
+
+function check()
+{
+var u=document.getElementById('text1').value;
+var p=document.getElementById('text2').value;
+if(u=="abc" && p=="123")
+{document.write("welcome");}
+else{document.write("enter correct password");}
+}
