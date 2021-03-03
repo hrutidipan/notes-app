279353a3fd22b61d11b941f1aa5ad1f43e853537
hrutidipan <hrutidipan@gmail.com>
2021-03-03 22:53:19 +0530

    made changes

--- (d)JS files/login.js
+++ (d)JS files/login.js
@@ -1,31 +1,37 @@
+function register() {
+  var email=document.getElementById('text1').Value;
+  var password=document.getElementById('text2').Value;
+  firebase.auth().createUserWithEmailAndPassword(email, password)
+  .then((userCredential) => {
+    // Signed in 
+    var user = userCredential.user;
+    // ...
+  })
+  .catch((error) => {
+    var errorCode = error.code;
+    var errorMessage = error.message;
 
-
-  // Your web app's Firebase configuration
-  // For Firebase JS SDK v7.20.0 and later, measurementId is optional
-  var firebaseConfig = {
-    apiKey: "AIzaSyDoqPlYgLEUUyirulXKMUULhAlj4ih3up0",
-    authDomain: "notesapp-efdd4.firebaseapp.com",
-    projectId: "notesapp-efdd4",
-    storageBucket: "notesapp-efdd4.appspot.com",
-    messagingSenderId: "678668896822",
-    appId: "1:678668896822:web:b149d646c41fc918ce2697",
-    measurementId: "G-J89YDXHP2Y"
-  };
-  // Initialize Firebase
-  firebase.initializeApp(firebaseConfig);
-  firebase.analytics();
- 
-  
-function register()
-{
-  var email=document.getElementById('text1').value;
-  var password=document.getElementById('text2').value;
-  firebase.auth().createUserWithEmailAndPassword(email,password).then(function(){
-    alert('signup done successfully');
-  }).catch(function(error)
-  {
-    var errorcode=error.code;
-    var errormsg=error.message;
+    
+    // ..
   });
+}
+
+function login(){
+  var useremail=document.getElementById('text1').Value;
+  var password=document.getElementById('text2').Value;
+
+  firebase.auth().signInWithEmailAndPassword(useremail, password)
+  .then((userCredential) => {
+    // Signed in
+    var user = userCredential.user;
+    window.alert("success");
+    // ...
+  })
+  .catch((error) => {
+    var errorCode = error.code;
+    var errorMessage = error.message;
+    window.alert("Error :" + errorMessage);
+  });
+  
 }
 
